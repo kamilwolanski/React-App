ef8f862e6714e2229ca46aa84e437862fe3231f7
kamilwolanski <kwolanski3@gmail.com>
2020-12-09 13:29:49 +0100

    Updates

---
+++ precache-manifest.ec842c624b0de102dc99d9c89430e6d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3441cd5c16bb8348cd3e",
+    "url": "/React-App/static/css/main.b832b0cd.chunk.css"
+  },
+  {
+    "revision": "3441cd5c16bb8348cd3e",
+    "url": "/React-App/static/js/main.b865585b.chunk.js"
+  },
+  {
+    "revision": "0a1cf9ef188380b4f4c6",
+    "url": "/React-App/static/js/runtime~main.a4b1c3b3.js"
+  },
+  {
+    "revision": "ceb1717918675d33a0f2",
+    "url": "/React-App/static/js/2.e01f3c02.chunk.js"
+  },
+  {
+    "revision": "70f51e3452a1c4a1302306ebde49f995",
+    "url": "/React-App/static/media/woman.70f51e34.jpg"
+  },
+  {
+    "revision": "ca0ea5217768135c27a6ba218d0eae2c",
+    "url": "/React-App/static/media/yoga.ca0ea521.jpg"
+  },
+  {
+    "revision": "1b5bc8d9b4da1fdf1e1d5303829b3ab9",
+    "url": "/React-App/static/media/carabao.1b5bc8d9.jpg"
+  },
+  {
+    "revision": "eba2d88e9be6eae6d0ff0d6b3c1bca89",
+    "url": "/React-App/index.html"
+  }
+];
